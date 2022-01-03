f0adcf88810f0d53736d6828ea3a518e5fece7fc
Stan Smith <stan@diamondfoundry.com>
2022-01-03 09:39:12 -0700

    Enable dynamic (user driven) search queries

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script>
+	$: q = '';
 	async function handleClick() {
-		const queryParams = new URLSearchParams({ q: 'spawn wildcats' });
+		const queryParams = new URLSearchParams({ q });
 		const response = await fetch(`/.netlify/functions/search?${queryParams}`);
 		console.log(await response.json());
 	}
@@ -9,6 +10,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
+<input type="text" bind:value={q} />
 <button on:click={handleClick}>Hello Function</button>
 
 <style>
